1f0c1384a7ef22a16c9a062433acf392e1394b14
heistermann <maik.heistermann@uni-potsdam.de>
2022-03-07 13:28:36 +0100

    update

--- make_html.sh
+++ make_html.sh
@@ -1,1 +1,1 @@
-jupyter nbconvert index.ipynb --output-dir='./docs' --TagRemovePreprocessor.remove_input_tags='{"hide-input"}' --TagRemovePreprocessor.remove_cell_tags='{"remove-cell"}' --no-prompt
+jupyter nbconvert index.ipynb --TagRemovePreprocessor.remove_input_tags='{"hide-input"}' --TagRemovePreprocessor.remove_cell_tags='{"remove-cell"}' --no-prompt
